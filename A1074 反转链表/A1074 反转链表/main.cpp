c4b0bb2f8e01cc30f580155d9153dc9342f4326c
ZhouAaron <zj971021@163.com>
2019-04-21 17:36:53 +0800

    PAT A1060 Are They equal

---
+++ A1074 反转链表/A1074 反转链表/main.cpp
@@ -0,0 +1,66 @@
+//
+//  main.cpp
+//  A1074 反转链表
+//
+//  Created by 周杰 on 2019/4/12.
+//  Copyright © 2019 周杰. All rights reserved.
+//
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+const int MAXN = 100010;
+struct NODE{
+    int data;
+    int address;
+    int next;
+    int flag;
+}node[MAXN];
+bool cmp(NODE a,NODE b){
+    return a.flag < b.flag;
+}
+int main(int argc, const char * argv[]) {
+    int begin,n,k,address;
+    scanf("%d %d %d",&begin,&n,&k);
+    for (int i = 0; i<MAXN; i++) {
+        node[i].flag = MAXN;
+    }
+    for (int i = 0; i<n; i++) {
+        scanf("%d",&address);
+        scanf("%d %d",&node[address].data,&node[address].next);
+        node[address].address = address;
+    }
+    int p = begin,count = 0;
+    while (p != -1) {
+        node[p].flag = count ++;
+        p = node[p].next;
+    }
+    sort(node,node+MAXN,cmp);
+    n = count;
+    for (int i = 0; i<n/k; i++) {
+        for (int j = (i + 1)*k-1; j>i*k; j--) {
+            printf("%05d %d %05d\n",node[j].address,node[j].data,node[j-1].address);
+        }
+        printf("%05d %d ",node[i *k].address,node[i*k].data);
+        if (i<n/k-1) {
+            printf("%05d\n",node[(i+2)*k-1].address);
+        }
+        else{
+            if (n%k==0) {
+                printf("-1\n");
+            }
+            else{
+                printf("%05d\n",node[(i+1)*k].address);
+                for (int i = n/k * k; i<n; i++) {
+                    printf("%05d %d",node[i].address,node[i].data);
+                    if (i<n-1) {
+                        printf("%05d\n",node[i+1].address);
+                    }else{
+                        printf("-1\n");
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
---
+++ PAT A1060 Are They Equal/PAT A1060 Are They Equal/main.cpp
@@ -0,0 +1,66 @@
+//
+//  main.cpp
+//  PAT A1060 Are They Equal
+//
+//  Created by 周杰 on 2019/4/21.
+//  Copyright © 2019 周杰. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+int n;//有效数字
+string deal(string s1,int &e){
+    int k = 0;//s的下标
+    while (s1[0]=='0'&&s1.length()>0) {
+        s1.erase(s1.begin());
+    }
+    if (s1[0] == '.') {
+        s1.erase(s1.begin());
+        while (s1[0] == '0' && s1.length()>0) {
+            s1.erase(s1.begin());
+            e--;
+        }
+    }else{
+        while (k<s1.length()&&s1[k]!='.') {
+            k++;
+            e++;
+        }
+        if (k<s1.length()) {
+            s1.erase(s1.begin()+k);
+        }
+    }
+    if (s1.length() == 0) {
+        e = 0;
+    }
+
+    int num = 0;
+    k = 0;
+    string res;
+    while (num < n) {
+        if (k<s1.length()) {
+            res += s1[k++];
+        }else{
+            res += '0';
+        }
+        num ++;
+    }
+     return res;
+}
+
+
+int main(int argc, const char * argv[]) {
+    string s1,s2,s3,s4;
+    cin>>n>>s1>>s2;
+    int e1 = 0,e2 = 0;
+    s3 = deal(s1,e1);
+    s4 = deal(s2,e2);
+    if (s3 == s4 && e1 == e2) {
+        cout<<"YES 0."<<s3<<"*10^"<<e1<<endl;
+    }else{
+        cout<<"NO 0."<<s3<<"*10^"<<e1<<"0."<<s4<<"*10^"<<e2<<endl;
+    }
+    
+    
+    return 0;
+}
