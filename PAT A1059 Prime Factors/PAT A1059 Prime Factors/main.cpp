f63aa6f5f47ea6ec06cc4d32fb32f1777d43edb1
ZhouAaron <zj971021@163.com>
2019-05-10 11:33:20 +0800

    PAT 1032

---
+++ PAT A1059 Prime Factors/PAT A1059 Prime Factors/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  PAT A1059 Prime Factors
+//
+//  Created by Aaron on 5/9/19.
+//  Copyright © 2019 周杰. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+`
